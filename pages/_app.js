2b59da447f29862bb3cbf362f3feaca1428b35a1
Alessandro Carinato <alessandrocarinato@Alessandros-MacBook-Pro.local>
2022-02-25 15:00:27 +0100

    first commit

---
+++ pages/_app.js
@@ -0,0 +1,9 @@
+// import '../styles/globals.css'
+// const bootstrap = require('bootstrap');
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />;
+}
+
+export default MyApp;
---
+++ pages/index.js
@@ -0,0 +1,14 @@
+const Home = () => {
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="column">
+          <h1 className="display-1 text-center py-5">giroq</h1>
+          {/* {JSON.stringify(state)} */}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
