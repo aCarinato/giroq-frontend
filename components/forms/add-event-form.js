871b70d7d3aec478420975b9b6dc92ed4741fbee
aCarinato <alessandro.carinato@gmail.com>
2022-03-21 11:39:14 +0100

    select type

--- components/forms/add-event-form.js
+++ components/forms/add-event-form.js
@@ -90,14 +90,23 @@ function AddEventForm(props) {
           <label className={classes.formCol} htmlFor="type">
             tipo
           </label>
-          <input
+          <select
+            className={classes.formInput}
+            name="type"
+            onChange={(e) => setType(e.target.value)}
+          >
+            <option value="">--Seleziona tipo--</option>
+            <option value="A">tipo A</option>
+            <option value="B">tipo B</option>
+          </select>
+          {/* <input
             className={classes.formInput}
             type="text"
             id="type"
             name="type"
             value={type}
             onChange={(e) => setType(e.target.value)}
-          />
+          /> */}
         </div>
         <div className={classes.formRow}>
           <label className={classes.formCol} htmlFor="event-date">
