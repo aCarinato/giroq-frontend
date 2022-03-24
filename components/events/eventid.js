0abb06d8b86985eecc4465f6456270e6080c4365
aCarinato <alessandro.carinato@gmail.com>
2022-03-24 22:52:17 +0100

    added ga script

--- components/events/eventid.js
+++ components/events/eventid.js
@@ -1,14 +1,18 @@
+import classes from './eventid.module.css';
+
 function EventId(props) {
   const { organiser, title, image } = props;
   return (
     <div>
       <h2>{title}</h2>
       <p>Organizzato da: {organiser}</p>
-      {image && (
-        <div>
-          <img src={image.url} alt={title} />
-        </div>
-      )}
+      <div className={classes.contenitore}>
+        {image && (
+          <div>
+            <img src={image.url} alt={title} />
+          </div>
+        )}
+      </div>
     </div>
   );
 }
