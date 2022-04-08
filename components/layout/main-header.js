57c1cc655c2f26358305b3323a58a7acebb267a9
aCarinato <alessandro.carinato@gmail.com>
2022-04-08 12:49:28 +0200

    added logo

--- components/layout/main-header.js
+++ components/layout/main-header.js
@@ -1,15 +1,19 @@
 import Link from 'next/link';
 import classes from './main.header.module.css';
+import Image from 'next/image';
 
 function MainHeader() {
   return (
     <header className={classes.header}>
       <div className={classes.logo}>
-        <Link href="/">giroq</Link>
+        <Link href="/">
+          {/* <Image src="/BRAND.png" alt="giroq" width="64" height="64" /> */}
+          <img src="/BRAND-pyr.svg" alt="giroq" />
+        </Link>
       </div>
-      <div className={classes.slogan}>
+      {/* <div className={classes.slogan}>
         <Link href="/">cose da fare attorno a te</Link>
-      </div>
+      </div> */}
     </header>
   );
 }
