50fbe0c04ee7219ff6ceb947788a64b1024b5a31
aCarinato <alessandro.carinato@gmail.com>
2022-05-14 13:43:47 +0200

    test bug og n2

--- pages/event/[eventId].js
+++ pages/event/[eventId].js
@@ -29,15 +29,34 @@ function EventDetailPage() {
   return (
     <>
       <Head>
+        <meta name="twitter:card" content="summary" />
+        <meta name="twitter:site" content="@nytimesbits" />
+        <meta name="twitter:creator" content="@nickbilton" />
+        <meta
+          property="og:url"
+          content="http://bits.blogs.nytimes.com/2011/12/08/a-twitter-for-my-sister/"
+        />
+        <meta property="og:title" content="A Twitter for My Sister" />
+        <meta
+          property="og:description"
+          content="In the early days, Twitter grew so quickly that it was almost impossible to add new features because engineers spent their time trying to keep the rocket ship from stalling."
+        />
+        <meta
+          property="og:image"
+          content="http://graphics8.nytimes.com/images/2011/12/08/technology/bits-newtwitter/bits-newtwitter-tmagArticle.jpg"
+        />
+
         {/* <title>{event.title}</title>
         <meta name="description" content={event.description} /> */}
-        <meta property="og:title" content="La mappa del tempo libero" />
+
+        {/* <meta property="og:title" content="La mappa del tempo libero" />
         <meta property="og:type" content="website" />
         <meta property="og:url" content="https://www.giroq.com/" />
         <meta
           property="og:image"
           content="https://www.veganok.com/wp-content/uploads/2020/08/vino.jpg"
-        />
+        /> */}
+
         {/* <meta property="og:title" content={event.title} />
         <meta property="og:type" content="website" />
         <meta
