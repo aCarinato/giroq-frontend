fece5d8ac4601ac2626cdec292ff765c277db352
aCarinato <alessandro.carinato@gmail.com>
2022-05-14 16:50:14 +0200

    test bug og n11

--- pages/event/[eventId].js
+++ pages/event/[eventId].js
@@ -4,32 +4,32 @@ import axios from 'axios';
 import EventId from '../../components/events/eventid';
 import Head from 'next/head';
 
-function EventDetailPage(props) {
+function EventDetailPage() {
   // console.log(props.event);
-  // const [event, setEvent] = useState({});
-  // const router = useRouter();
+  const [event, setEvent] = useState({});
+  const router = useRouter();
 
-  // const eventId = router.query.eventId;
+  const eventId = router.query.eventId;
 
-  // const fetchEvent = async () => {
-  //   try {
-  //     const { data } = await axios.get(
-  //       `${process.env.NEXT_PUBLIC_API}/event/${eventId}`
-  //     );
-  //     setEvent(data);
-  //   } catch (err) {
-  //     console.log(err);
-  //   }
-  // };
+  const fetchEvent = async () => {
+    try {
+      const { data } = await axios.get(
+        `${process.env.NEXT_PUBLIC_API}/event/${eventId}`
+      );
+      setEvent(data);
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
-  // useEffect(() => {
-  //   if (eventId) fetchEvent();
-  // }, [eventId]);
+  useEffect(() => {
+    if (eventId) fetchEvent();
+  }, [eventId]);
 
   return (
     <>
       <Head>
-        <meta property="og:title" content={props.event.title} />
+        {/* <meta property="og:title" content={props.event.title} />
         <meta
           property="og:url"
           content={`https://www.giroq.com/event/${props.event._id}/`}
@@ -38,48 +38,48 @@ function EventDetailPage(props) {
           property="og:image"
           content={props.event.image && props.event.image.url}
         />
-        <meta property="og:description" content={props.event.description} />
+        <meta property="og:description" content={props.event.description} /> */}
       </Head>
-      {/* <EventId
-        organiser={props.event.organiser}
-        title={props.event.title}
-        description={props.event.description}
-        image={props.event.image}
-        street={props.event.street}
-        city={props.event.city}
-        startDate={props.event.startDate}
-        endDate={props.event.endDate}
-        start={props.event.startTime}
-        end={props.event.endTime}
-        link={props.event.link}
-      /> */}
-      <div className="textTest">{props.event.organiser}</div>
+      <EventId
+        organiser={event.organiser}
+        title={event.title}
+        description={event.description}
+        image={event.image}
+        street={event.street}
+        city={event.city}
+        startDate={event.startDate}
+        endDate={event.endDate}
+        start={event.startTime}
+        end={event.endTime}
+        link={event.link}
+      />
+      {/* <div className="textTest">{props.event.organiser}</div>
       <div className="textTest">{props.event.title}</div>
       <div className="textTest">{props.event.description}</div>
-      <div style={{ color: 'green' }}>{props.event.link}</div>
+      <div style={{ color: 'green' }}>{props.event.link}</div> */}
     </>
   );
 }
 
 export default EventDetailPage;
 
-export async function getServerSideProps(context) {
-  const { params, req, res } = context;
+// export async function getServerSideProps(context) {
+//   const { params, req, res } = context;
 
-  const eventId = params.eventId;
+//   const eventId = params.eventId;
 
-  try {
-    const { data } = await axios.get(
-      `${process.env.NEXT_PUBLIC_API}/event/${eventId}`
-    );
+//   try {
+//     const { data } = await axios.get(
+//       `${process.env.NEXT_PUBLIC_API}/event/${eventId}`
+//     );
 
-    return {
-      props: {
-        event: data,
-      },
-    };
-    // setEvent(data);
-  } catch (err) {
-    console.log(err);
-  }
-}
+//     return {
+//       props: {
+//         event: data,
+//       },
+//     };
+//     // setEvent(data);
+//   } catch (err) {
+//     console.log(err);
+//   }
+// }
