0d88a009dea5ce8497929aa6febed5387374b2c2
aCarinato <alessandro.carinato@gmail.com>
2022-05-14 14:02:30 +0200

    test bug og n3

--- pages/event/[eventId].js
+++ pages/event/[eventId].js
@@ -41,10 +41,11 @@ function EventDetailPage() {
           property="og:description"
           content="In the early days, Twitter grew so quickly that it was almost impossible to add new features because engineers spent their time trying to keep the rocket ship from stalling."
         />
-        <meta
+        <meta property="og:image" content={event.image && event.image.url} />
+        {/* <meta
           property="og:image"
           content="http://graphics8.nytimes.com/images/2011/12/08/technology/bits-newtwitter/bits-newtwitter-tmagArticle.jpg"
-        />
+        /> */}
 
         {/* <title>{event.title}</title>
         <meta name="description" content={event.description} /> */}
